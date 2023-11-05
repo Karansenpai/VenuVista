95a726f25cdae9bab7b41d626aa64dea54ffe0b8
parimal-10 <amrutkar.parimal@gmail.com>
2023-11-05 08:29:15 +0530

    24

--- Frontend/src/components/HotelPage.jsx
+++ Frontend/src/components/HotelPage.jsx
@@ -11,24 +11,36 @@ import axios from "axios";
 
 
 export default function HotelPage() {
-
-  const [data, setData] = React.useState({});
-  React.useEffect(()=>{
-    const init = async() =>{
-      const response = await axios.get(
-        `${BASE_URL}/api/fetch/fetchone`,
-        {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        }
-      );
-      setData(response.data);
-    }
-    init();
-  },[])
   const [seats, setseats] = React.useState(0);
   const [vacate, setvacate] = React.useState(0);
+  const [totalTables ,settotalTable] = React.useState(0);
+  const [tableOccupied ,settableOccupied] = React.useState(0);
+  const [refersh, setrefresh] = React.useState(1);
+  const [data, setData] = React.useState({});
+
+  React.useEffect(()=>{
+    const init = async () => {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}/api/fetch/fetchone`,
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        );
+        const responseData = response.data;
+        console.log(responseData);
+        settotalTable(responseData.totalTables);
+        settableOccupied(responseData.tablesOccupied);
+        setData(responseData);
+      } catch (error) {
+        // Handle any potential errors here
+        console.error("Error fetching data:", error);
+      }
+    };
+    init();    
+  },[refersh])
   return (
     <div style={{marginTop: '16px', marginBottom: '16px', display: "flex", direction: "column", alignItems: "center", justifyContent: 'center'}}>
     <Card sx={{ width: '80%'}}>
@@ -47,8 +59,23 @@ export default function HotelPage() {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      <CardActions style={{display: 'flex', flexDirection: 'column'}}>
         <Button variant="outlined">Edit</Button>
+        <div style={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+          <div>
+        <Typography gutterBottom variant="h5" component="div">
+          Total seats: {totalTables}
+          </Typography>
+          </div>
+          <div>
+          <Typography gutterBottom variant="h5" component="div">
+          Available seats: {totalTables-tableOccupied}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+          Occupued seats: {tableOccupied}
+          </Typography>
+          </div>
+          </div>
       </CardActions>
       <div style={{marginTop: '16px', marginBottom: '16px', display: "flex", direction: "column", alignItems: "center", justifyContent: 'center', gap: '20px'}}>
       <TextField id="filled-basic" label="no of tables" variant="filled" 
@@ -74,6 +101,7 @@ export default function HotelPage() {
             }
           );
           if (response.data.message) {
+            setrefresh(refersh+1);
             alert(response.data.message);
           } else {
             alert(response.data.err);
@@ -102,6 +130,7 @@ export default function HotelPage() {
                 }
               );
               if (response.data.message) {
+                setrefresh(refersh+1);
                 alert(response.data.message);
               } else {
                 alert(response.data.err);
